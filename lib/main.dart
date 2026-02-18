893c768600751240b4322fe1528e547363b82840
Ricardo <rk.draw2020@gmail.com>
2026-02-18 15:18:38 -0300

    atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,220 @@
 import 'package:flutter/material.dart';
+import 'database/database_helper.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Cadastro SQLite",
+
+      debugShowCheckedModeBanner: false,
+
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  TextEditingController textoController = TextEditingController();
 
-  void _incrementCounter() {
+  TextEditingController numeroController = TextEditingController();
+
+  List<Map<String, dynamic>> lista = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    carregar();
+  }
+
+  Future carregar() async {
+    final dados = await DatabaseHelper.listar();
+
+    print("DADOS CARREGADOS:");
+    print(dados);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      lista = dados;
     });
+  }
+
+  Future inserir() async {
+    try {
+      final texto = textoController.text.trim();
+
+      final numero = int.parse(numeroController.text.trim());
+
+      await DatabaseHelper.inserir(texto, numero);
+
+      textoController.clear();
+      numeroController.clear();
+
+      await carregar();
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Inserido com sucesso")));
+    } catch (e) {
+      print(e);
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Erro: $e")));
+    }
+  }
+
+  Future deletar(int id) async {
+    await DatabaseHelper.deletar(id);
+
+    await carregar();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: Text("Sistema de Cadastro")),
+
+      body: Padding(
+        padding: EdgeInsets.all(20),
+
+        child: SizedBox.expand(
+          child: Column(
+            children: [
+              TextField(
+                controller: textoController,
+
+                decoration: InputDecoration(labelText: "Campo Texto"),
+              ),
+
+              TextField(
+                controller: numeroController,
+
+                keyboardType: TextInputType.number,
+
+                decoration: InputDecoration(labelText: "Campo Número"),
+              ),
+
+              SizedBox(height: 10),
+
+              ElevatedButton(onPressed: inserir, child: Text("Inserir")),
+
+              SizedBox(height: 20),
+
+              Expanded(
+                child: lista.isEmpty
+                    ? Center(child: Text("Nenhum cadastro ainda"))
+                    : ListView.builder(
+                        itemCount: lista.length,
+
+                        itemBuilder: (context, index) {
+                          var item = lista[index];
+
+                          return Card(
+                            child: ListTile(
+                              title: Text(item["texto"].toString()),
+
+                              subtitle: Text(
+                                "Número: " + item["numero"].toString(),
+                              ),
+
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.edit),
+
+                                    onPressed: () {
+                                      mostrarDialogEditar(item);
+                                    },
+                                  ),
+
+                                  IconButton(
+                                    icon: Icon(Icons.delete),
+
+                                    onPressed: () => deletar(item["id"]),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  void mostrarDialogEditar(Map<String, dynamic> item) {
+    final textoEdit = TextEditingController(text: item["texto"]);
+
+    final numeroEdit = TextEditingController(text: item["numero"].toString());
+
+    showDialog(
+      context: context,
+
+      builder: (_) {
+        return AlertDialog(
+          title: Text("Editar"),
+
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+
+            children: [
+              TextField(
+                controller: textoEdit,
+                decoration: InputDecoration(labelText: "Texto"),
+              ),
+
+              TextField(
+                controller: numeroEdit,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Número"),
+              ),
+            ],
+          ),
+
+          actions: [
+            TextButton(
+              child: Text("Cancelar"),
+
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+
+            TextButton(
+              child: Text("Salvar"),
+
+              onPressed: () async {
+                await DatabaseHelper.atualizar(
+                  item["id"],
+
+                  textoEdit.text,
+
+                  int.parse(numeroEdit.text),
+                );
+
+                Navigator.pop(context);
+
+                carregar();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
